985660807eaf2dfcb1d323c7c4b96999ceaed577
daheeahn <deg9810@gmail.com>
2022-11-11 19:03:24 +0900

    [part2-ch4-2-내장 갤러리에서 사진 선택하여 추가하기 - ImagePicker] 최종 코드

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+
+import { useGallery } from "./src/use-gallery";
+
+const width = Dimensions.get("screen").width;
+const columnSize = width / 3;
 
 export default function App() {
+  const { images,imagesWithAddButton, pickImage, deleteImage } = useGallery();
+
+  const onPressOpenGallery = () => {
+    pickImage();
+  };
+  const onLongPressImage = (iamgeId) => deleteImage(iamgeId);
+
+  const renderItem = ({ item: { id, uri }, index }) => {
+    if (id === -1) {
+      return (
+        <TouchableOpacity 
+          onPress={onPressOpenGallery}
+          style={{
+            width: columnSize, 
+            height: columnSize, 
+            backgroundColor: "lightgrey",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Text style={{ fontWeight: "100", fontSize: 45 }}>+</Text>    
+        </TouchableOpacity>
+      )
+    }
+    return (
+      <TouchableOpacity onLongPress={() => onLongPressImage(id)}>
+        <Image
+          source={{ uri }}
+          style={{ width: columnSize, height: columnSize }}
+        />
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList data={imagesWithAddButton} renderItem={renderItem} numColumns={3} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
 });
