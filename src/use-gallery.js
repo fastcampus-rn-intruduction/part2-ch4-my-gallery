969d4ac7e487216a3f6a0dd53519e205cf30f4eb
daheeahn <deg9810@gmail.com>
2022-11-11 23:00:04 +0900

    [part2-ch4-6-앨범별로 추가한 사진들을 AsyncStorage에 저장하고 불러오기] 최종 코드

--- src/use-gallery.js
+++ src/use-gallery.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const defaultAlbum = {
   id: 1,
   title: "기본",
+}
+
+const ASYNC_KEY = {
+  IMAGES: 'images',
+  ALBUMS: 'albums',
 }
 
 export const useGallery = () => {
@@ -16,6 +22,15 @@ export const useGallery = () => {
   const [albumTitle, setAlbumTitle] = useState('');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+
+  const _setImages = (newImages) => {
+    setImages(newImages);
+    AsyncStorage.setItem(ASYNC_KEY.IMAGES, JSON.stringify(newImages));
+  };
+  const _setAlbums = (newAlbums) => {
+    setAlbums(newAlbums);
+    AsyncStorage.setItem(ASYNC_KEY.ALBUMS, JSON.stringify(newAlbums));
+  };
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -35,7 +50,7 @@ export const useGallery = () => {
         uri: result.assets[0].uri,
         albumId: selectedAlbum.id,
       };
-      setImages([...images, newImage]);
+      _setImages([...images, newImage]);
     }
   };
 
@@ -49,7 +64,7 @@ export const useGallery = () => {
         text: "네",
         onPress: () => {
           const newImages = images.filter((image) => image.id !== imageId);
-          setImages(newImages);
+          _setImages(newImages);
         },
       },
     ]);
@@ -68,7 +83,7 @@ export const useGallery = () => {
       id: lastId + 1,
       title: albumTitle,
     };
-    setAlbums([
+    _setAlbums([
       ...albums,
       newAlbum,
     ]);
@@ -91,7 +106,7 @@ export const useGallery = () => {
         text: "네",
         onPress: () => {
           const newAlbums = albums.filter((album) => album.id !== albumId);
-          setAlbums(newAlbums);
+          _setAlbums(newAlbums);
           setSelectedAlbum(defaultAlbum);
         },
       },
@@ -124,10 +139,10 @@ export const useGallery = () => {
     setSelectedImage(nextImage);
   };
 
-  // const showPreviousArrow = filteredImages.findIndex(image => image.id === selectedImage?.id) !== 0;
-  // const showNextArrow = filteredImages.findIndex(image => image.id === selectedImage?.id) !== filteredImages.length - 1;
-  const showPreviousArrow = true;
-  const showNextArrow = true;
+  const showPreviousArrow = filteredImages.findIndex(image => image.id === selectedImage?.id) !== 0;
+  const showNextArrow = filteredImages.findIndex(image => image.id === selectedImage?.id) !== filteredImages.length - 1;
+  // const showPreviousArrow = true;
+  // const showNextArrow = true;
 
   const resetAlbumTitle = () => setAlbumTitle('');
 
@@ -137,13 +152,25 @@ export const useGallery = () => {
       id: -1,
       uri: "",
     }
-  ]
+  ];
+
+  const initValues = async () => {
+    // images
+    const imagesFromStorage = await AsyncStorage.getItem(ASYNC_KEY.IMAGES);
+    if (imagesFromStorage !== null) {
+      const parsed = JSON.parse(imagesFromStorage);
+      setImages(parsed);
+    }
+    // albums
+    const albumsFromStorage = await AsyncStorage.getItem(ASYNC_KEY.ALBUMS);
+    if (albumsFromStorage !== null) {
+      const parsed = JSON.parse(albumsFromStorage);
+      setAlbums(parsed);
+    }
+  };
 
   useEffect(() => {
-    // console.log('1) images', images);
-    // const arr = [1, 2, 3];
-    // const index = arr.findIndex(item => item === 4);
-    // console.log('index', index);
+    initValues();
   }, []);
 
   return {
