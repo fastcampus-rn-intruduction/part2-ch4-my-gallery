b79dbb4c7fd1b6662d95431afc7d439855c807ac
daheeahn <deg9810@gmail.com>
2022-11-12 17:12:18 +0900

    [part2-ch4-9-정리] 최종 코드

--- src/TextInputModal.js
+++ src/TextInputModal.js
@@ -2,27 +2,27 @@ import { KeyboardAvoidingView, Modal, Pressable, SafeAreaView, TextInput, View }
 
 export default ({ modalVisible, albumTitle, setAlbumTitle, onSubmitEditing, onPressBackdrop }) => {
   return (
-    <Modal 
-      animationType="fade"
-      transparent={true}
-      visible={modalVisible}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{ flex: 1 }}>
-        <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
-          
-          <SafeAreaView style={{ flex: 1, width: "100%", position:"absolute", bottom: 0 }}>
-            <TextInput 
-              placeholder="앨범명을 입력해주세요"
-              style={{ width: "100%", padding: 10, borderWidth: 0.5, borderColor: "lightgrey" }}
-              value={albumTitle}
-              onChangeText={setAlbumTitle}
-              onSubmitEditing={onSubmitEditing}
-              autoFocus={true}
-            />
-          </SafeAreaView>
-        </Pressable>
-      </KeyboardAvoidingView>
-    </Modal>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{  }}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}>
+          <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
+            
+            <SafeAreaView style={{ flex: 1, width: "100%", position:"absolute", bottom: 0 }}>
+              <TextInput 
+                placeholder="앨범명을 입력해주세요"
+                style={{ width: "100%", padding: 10, borderWidth: 0.5, borderColor: "lightgrey", backgroundColor: "white" }}
+                value={albumTitle}
+                onChangeText={setAlbumTitle}
+                onSubmitEditing={onSubmitEditing}
+                autoFocus={true}
+              />
+            </SafeAreaView>
+          </Pressable>
+      </Modal>
+    </KeyboardAvoidingView>
   )
 }
