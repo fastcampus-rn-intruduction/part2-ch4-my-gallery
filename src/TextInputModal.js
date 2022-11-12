c3f684352081ef8962b6a2ccc98c4362eb31dfe7
daheeahn <deg9810@gmail.com>
2022-11-12 17:43:51 +0900

    [part2-ch4-9-정리] 최종 코드 (2) - KeyboardAvoidingView 대응 - iOS에서는 Modal 다음에 KeyboardAvoidingView가 위치해있어야 제대로 동작함

--- src/TextInputModal.js
+++ src/TextInputModal.js
@@ -1,28 +1,79 @@
-import { KeyboardAvoidingView, Modal, Pressable, SafeAreaView, TextInput, View } from "react-native"
+import {
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 
-export default ({ modalVisible, albumTitle, setAlbumTitle, onSubmitEditing, onPressBackdrop }) => {
+const Content = ({
+  albumTitle,
+  setAlbumTitle,
+  onSubmitEditing,
+  onPressBackdrop,
+}) => {
+  return (
+    <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
+      <SafeAreaView
+        style={{ flex: 1, width: "100%", position: "absolute", bottom: 0 }}
+      >
+        <TextInput
+          placeholder="앨범명을 입력해주세요"
+          style={{
+            width: "100%",
+            padding: 10,
+            borderWidth: 0.5,
+            borderColor: "lightgrey",
+            backgroundColor: "white",
+          }}
+          value={albumTitle}
+          onChangeText={setAlbumTitle}
+          onSubmitEditing={onSubmitEditing}
+          autoFocus={true}
+        />
+      </SafeAreaView>
+    </Pressable>
+  );
+};
+
+export default ({
+  modalVisible,
+  albumTitle,
+  setAlbumTitle,
+  onSubmitEditing,
+  onPressBackdrop,
+}) => {
+  if (Platform.OS === "ios") {
+    return (
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+        >
+          <Content
+            albumTitle={albumTitle}
+            setAlbumTitle={setAlbumTitle}
+            onSubmitEditing={onSubmitEditing}
+            onPressBackdrop={onPressBackdrop}
+          />
+        </KeyboardAvoidingView>
+      </Modal>
+    );
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{  }}>
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}>
-          <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
-            
-            <SafeAreaView style={{ flex: 1, width: "100%", position:"absolute", bottom: 0 }}>
-              <TextInput 
-                placeholder="앨범명을 입력해주세요"
-                style={{ width: "100%", padding: 10, borderWidth: 0.5, borderColor: "lightgrey", backgroundColor: "white" }}
-                value={albumTitle}
-                onChangeText={setAlbumTitle}
-                onSubmitEditing={onSubmitEditing}
-                autoFocus={true}
-              />
-            </SafeAreaView>
-          </Pressable>
+      style={{}}
+    >
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <Content
+          albumTitle={albumTitle}
+          setAlbumTitle={setAlbumTitle}
+          onSubmitEditing={onSubmitEditing}
+          onPressBackdrop={onPressBackdrop}
+        />
       </Modal>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
